3376699758248af3de680b47d251432b9ddb9314
iBat <alex@bulychev.net>
2012-05-06 22:17:45 +0400

    refactoring

--- proxy.js
+++ proxy.js
@@ -22,10 +22,80 @@ db.open(function(error, client) {
     collection = new mongo.Collection(client, collectionName);
 });
 
-var updateDb = function(players) {
-    players.forEach(function(player) {
-        if(player.eff !== "X")
-            collection.update({ id: player.id }, player, { upsert: true });
+http.createServer(function(request, response) {
+    var query = getQuery(request, response);
+
+    if(!query)
+        return;
+
+    var ids = [ ],
+        parts = query.split(",");
+
+    parts.forEach(function(part) {
+        ids.push(parseInt(part));
+    });
+
+    processLocal(ids, response);
+}).listen(1337, "127.0.0.1");
+
+utils.log("Server running at http://127.0.0.1:1337/");
+
+var getQuery = function(request, response) {
+    var query;
+
+    try {
+        query = url.parse(request.url).query;
+    } catch(e) {
+        response.statusCode = 500;
+        response.end("wrong request");
+        utils.log("url.parse error: " + request.url);
+        return false;
+    }
+
+    if(!(query && query.match(/^\d(\d|,)+\d$/))) {
+        response.statusCode = 500;
+        response.end("wrong request");
+        utils.log("query match error: " + query);
+        return false;
+    }
+
+    return query;
+};
+
+var processLocal = function(ids, response) {
+    var inCache = [ ],
+        forUpdate = [ ],
+        checks = [ ],
+        now = new Date();
+
+    ids.forEach(function(id) {
+        checks.push(function(callback) {
+            collection.find({ id: id }, { limit: 1 }).toArray(function(error, records) {
+                if(error) {
+                    callback(error);
+                    return;
+                }
+
+                var playerRecord = records.length && records[0];
+
+                if(playerRecord && (now - playerRecord.date) < cacheTtl)
+                    inCache.push(playerRecord);
+                else
+                    forUpdate.push(id);
+
+                callback(null);
+            });
+        });
+    });
+
+    async.series(checks, function(error) {
+        if(error) {
+            response.statusCode = 500;
+            response.end("DB connection error");
+            utils.log("DB connection error 3");
+            return;
+        }
+        processRemotes(inCache, forUpdate, response);
     });
 };
 
@@ -37,7 +107,7 @@ var processRemotes = function(inCache, forUpdate, response) {
             var options = {
                 host: "worldoftanks.ru",
                 port: 80,
-                path: "/uc/accounts/" + id + "/api/1.2/?source_token=Intellect_Soft-WoT_Mobile-unofficial_stats"
+                path: "/uc/accounts/" + id + "/api/1.3/?source_token=Intellect_Soft-WoT_Mobile-unofficial_stats"
             };
 
             var reqTimeout = setTimeout(function() {
@@ -116,75 +186,9 @@ var processRemotes = function(inCache, forUpdate, response) {
     });
 };
 
-var getQuery = function(request, response) {
-    var query;
-
-    try {
-        query = url.parse(request.url).query;
-    } catch(e) {
-        response.statusCode = 500;
-        response.end("wrong request");
-        utils.log("url.parse error: " + request.url);
-        return false;
-    }
-
-    if(!(query && query.match(/^\d(\d|,)+\d$/))) {
-        response.statusCode = 500;
-        response.end("wrong request");
-        utils.log("query match error: " + query);
-        return false;
-    }
-
-    return query;
+var updateDb = function(players) {
+    players.forEach(function(player) {
+        if(player.eff !== "X")
+            collection.update({ id: player.id }, player, { upsert: true });
+    });
 };
-
-http.createServer(function(request, response) {
-	var query = getQuery(request, response);
-
-    if(!query)
-        return;
-
-    var ids = [ ],
-        inCache = [ ],
-        forUpdate = [ ],
-        parts = query.split(","),
-        checks = [ ],
-        now = new Date();
-
-    parts.forEach(function(part) {
-        ids.push(parseInt(part));
-    });
-
-    ids.forEach(function(id) {
-        checks.push(function(callback) {
-            collection.find({ id: id }, { limit: 1 }).toArray(function(error, records) {
-                if(error) {
-                    callback(error);
-                    return;
-                }
-
-                var playerRecord = records.length && records[0];
-
-                if(playerRecord && (now - playerRecord.date) < cacheTtl)
-                    inCache.push(playerRecord);
-                else
-                    forUpdate.push(id);
-
-                callback(null);
-            });
-        });
-    });
-
-    async.series(checks, function(error) {
-        if(error) {
-            response.statusCode = 500;
-            response.end("DB connection error");
-            utils.log("DB connection error 3");
-            return;
-        }
-        processRemotes(inCache, forUpdate, response);
-    });
-	
-}).listen(1337, "127.0.0.1");
-
-utils.log("Server running at http://127.0.0.1:1337/");
